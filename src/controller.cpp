a130d4cc025e0a7a1a603e4290e90f50ff7931c0
Yair Krothamer <yairkr@edu.hac.ac.il>
2025-10-19 15:12:14 +0300

    First Board

---
+++ src/controller.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include "controller.h"
+
+void Controller::startGame() {
+    std::cout << "============================\n";
+    std::cout << "      המשחק התחיל! 🎮\n";
+    std::cout << "============================\n";
+}
